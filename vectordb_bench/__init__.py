a53bb8c0e15169d60bcbaac333764ab3f9e678ce
noahstaveley <hskate98@gmail.com>
2026-02-10 14:20:00 -0800

    Increase batch size to 1000 for benchmarking

--- vectordb_bench/__init__.py
+++ vectordb_bench/__init__.py
@@ -19,7 +19,8 @@ class config:
     DEFAULT_DATASET_URL = env.str("DEFAULT_DATASET_URL", AWS_S3_URL)
     DATASET_SOURCE = env.str("DATASET_SOURCE", "S3")  # Options "S3" or "AliyunOSS"
     DATASET_LOCAL_DIR = env.path("DATASET_LOCAL_DIR", "/tmp/vectordb_bench/dataset")
-    NUM_PER_BATCH = env.int("NUM_PER_BATCH", 100)
+    #NUM_PER_BATCH = env.int("NUM_PER_BATCH", 100)
+    NUM_PER_BATCH = env.int("NUM_PER_BATCH", 1000)
     TIME_PER_BATCH = 1  # 1s. for streaming insertion.
     MAX_INSERT_RETRY = 5
     MAX_SEARCH_RETRY = 5
